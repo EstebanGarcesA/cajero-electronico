99fc06e97e7cda1bf79b0ed6d53687c271632ffd
EstebanGarcesA <esteg254@gmail.com>
2025-05-28 19:51:52 -0500

    Se realizan los puntos 4,5,7

--- src/App.java
+++ src/App.java
@@ -13,14 +13,58 @@ public class App {
 
             if (usuario.equals("admin") && contrasena.equals("1234")){
                 System.out.println(usuario + ". Bienvenido al cajero electronico");
-                double saldoCuenta = 0;
-                System.out.println("Seleccione una opcion: \n1. Consultar saldo\n2. Retirar Dinero\n3. Depositar Dinero\n4. Transferir dinero\n5. Cerrar Sesión");
-                int opcion = sc.nextInt();
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Su saldo es: "+ saldoCuenta);
-                        break;
-                }
+
+                int opcion = 0;
+                double saldoCuenta = 11000;
+                do {
+                    System.out.println("Seleccione una opcion: \n1. Consultar saldo\n2. Retirar Dinero\n3. Transferir dinero\n4. Depositar dinero\n5. Cerrar Sesión");
+                    opcion = sc.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Su saldo es: "+ saldoCuenta);
+                            break;
+                        case 2:
+                            System.out.print("Ingrese el valor a retirar: ");
+                            double valorRetiro = sc.nextDouble();
+                            if (valorRetiro >= saldoCuenta){
+                                System.out.println("El saldo " + valorRetiro + " supera el valor que tiene en la cuenta" );
+                            } else {
+                                if (saldoCuenta - valorRetiro < 1){
+                                    System.out.println("La cuenta no puede quedar en 0");
+                                }else {
+                                    saldoCuenta -= valorRetiro;
+                                    System.out.println("Retiro exitoso. su nuevo saldo es: "+saldoCuenta);
+                                }
+
+                            }
+                            break;
+                        case 3:
+                            System.out.print("Ingrese el valor a transferir: ");
+                            double valorTransferencia = sc.nextDouble();
+                            System.out.println("Ingrese el numero de cuenta a la que va a transferir");
+
+                            if (valorTransferencia >= saldoCuenta){
+                                System.out.println("El saldo " + valorTransferencia + " supera el valor que tiene en la cuenta" );
+                            } else {
+                                if (saldoCuenta - valorTransferencia < 1){
+                                    System.out.println("La cuenta no puede quedar en 0");
+                                }else {
+                                    saldoCuenta -= valorTransferencia;
+                                    System.out.println("Transferencia exitosa a la cuenta X. su nuevo saldo es: "+saldoCuenta);
+                                }
+
+                            }
+                            break;
+                        case 5:
+                            System.out.println("Cerrando sesión...");
+                            intentos = 3;
+                            break;
+
+
+                        default:
+                            break;
+                    }
+                }while(opcion != 5);
             } else {
                 System.out.println("Usuario y/o contraseña incorrectos");
                 intentos++;
